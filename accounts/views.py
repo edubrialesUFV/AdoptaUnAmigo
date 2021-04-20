f29ebaa989f8c56c19185cd30720d9cb0c5f4c1c
edubrialesufv <edudu@pop-os.localdomain>
2021-04-20 21:36:12 +0200

    login automatico despues de registrarse

--- accounts/views.py
+++ accounts/views.py
@@ -2,14 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .forms import RegisterForm
 from django.urls import reverse_lazy, reverse
-from django.contrib.auth import logout
+from django.contrib.auth import logout, authenticate, login
 # Create your views here.
 
 def register(response):    
     if response.method == 'POST':
         form = RegisterForm(response.POST)
         if form.is_valid():
-            form.save()
+            new_user=form.save()
+            #Login automatico despues de registrarse
+            new_user = authenticate(username=form.cleaned_data['username'],
+                                    password=form.cleaned_data['password1'],
+                                    )
+            login(response, new_user)
             return HttpResponseRedirect(reverse('home'))
     else:
         form = RegisterForm()
