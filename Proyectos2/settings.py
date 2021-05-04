ef91d5560413199a1d481eadb9af625db906230a
edubrialesufv <edudu@pop-os.localdomain>
2021-05-04 23:32:11 +0200

    mergeando 6

--- Proyectos2/settings.py
+++ Proyectos2/settings.py
@@ -48,13 +48,13 @@ INSTALLED_APPS = [
     #Nuestras apps
     'AdoptaUnAmigo.apps.AdoptaunamigoConfig',
     'accounts',
-    #'django_extensions',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-   # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
